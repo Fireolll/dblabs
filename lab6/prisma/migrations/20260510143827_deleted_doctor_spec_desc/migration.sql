b0218730fa4495984c1e74166b1617ce2d1d666b
Fireolll <79473655+Fireolll@users.noreply.github.com>
2026-05-10 20:57:14 +0300

    deleted wrong migration logs

--- lab6/prisma/migrations/20260510143827_deleted_doctor_spec_desc/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `description` on the `doctor_specialization` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "doctor_specialization" DROP COLUMN "description";
